4ef136a1a7b3aa6ed069db7ae154288b756d3be2
Sergio <ssancd03@estudiantes.unileon.es>
2022-05-08 20:26:11 +0200

    Vistas Admin 2.1

--- src/main/java/controller/IndexController.java
+++ src/main/java/controller/IndexController.java
@@ -35,11 +35,46 @@ public class IndexController implements Serializable{
     public String moveToPrueba(){
         return "prueba";
     }
+<<<<<<< Updated upstream
     public void insertarUsuario(){
         try{
             userEJB.create(user);
         }catch(Exception e){
             System.out.println("Error al insertar el usuario en la base de datos "+ e.getMessage());
+=======
+    
+    public String moveToIndex(){
+        return "index";
+    }
+
+    public String moveToIndexAdmin(){
+        return "indexAdmin";
+    }
+    public String moveToCrearPlan(){
+        return "createPlanAdmin";
+    }
+    public String moveToAdmin(){
+        return "indexAdmin";
+    }
+    /* REGISTER IMPLEMENTATION */
+    public String insertarUsuario() {
+        
+        String plainPassword = user.getPassword();
+        String hashPassword = hashPassword(plainPassword);
+        user.setPassword(hashPassword);
+        user.setType("C");
+        user.setRol(rolEJB.getRol(user.getType()));
+        client.setUser(user);
+
+        try {
+            clientEJB.create(client);
+            FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("cliente", client);
+            return "home";
+        } catch (Exception e) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "Error al crear el usuario"));
+            System.out.println("Error: "+e.getMessage());
+            return "index";
+>>>>>>> Stashed changes
         }
     }
 
